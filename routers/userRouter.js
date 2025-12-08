ce8e1fe0231b332b266c715cca6c9e23586f5718
Favour <georgechiamaka02@gmail.com>
2025-12-08 10:43:31 +0100

    added swagger to register route

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,6 +3,61 @@ const userRouter = express.Router();
 const { registerUser, loginUser } = require('../controllers/userController');
 const Uploads = require('../middlewares/uploads');
 
+/**
+* @swagger
+* /registerUser:
+*   post:
+*     summary: Register a new user
+*     tags:
+*       - User
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             required:
+*               - firstname
+*               - lastname
+*               - email
+*               - phone
+*               - address
+*               - password
+*               - confirmpassword
+*             properties:
+*               firstname:
+*                 type: string
+*                 description: User's first name
+*               lastname:
+*                 type: string
+*                 description: User's last name
+*               email:
+*                 type: string
+*                 format: email
+*                 description: User's email address
+*               phone:
+*                 type: string
+*                 description: User's phone number
+*               address:
+*                 type: string
+*                 description: User's address
+*               password:
+*                 type: string
+*                 format: password
+*                 description: User's password
+*               confirmpassword:
+*                 type: string
+*                 format: password
+*                 description: Password confirmation
+*               image:
+*                 type: string
+*                 description: User's profile image (URL or base64 encoded string)
+*     responses:
+*       200:
+*         description: Registration successful
+*       400:
+*         description: Validation error - missing required field
+*/
 userRouter.post("/registerUser", Uploads.single("image"), registerUser);
 
 /**
