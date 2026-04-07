1f53306bdceddc98ccba90bebea31cd6e49cf6d3
Favour <georgechiamaka02@gmail.com>
2026-04-08 00:32:58 +0100

    updated payment conntroller

--- routers/paymentRouter.js
+++ routers/paymentRouter.js
@@ -4,5 +4,5 @@ const paymentRouter = express.Router();
 const { initializePayment, verifyPayment } = require("../controllers/paymentController");
 
 paymentRouter.post("/initializePayment", isUser, initializePayment);
-paymentRouter.post("/verifyPayment", isUser, verifyPayment);
+paymentRouter.get("/verifyPayment", isUser, verifyPayment);
 module.exports = paymentRouter;
