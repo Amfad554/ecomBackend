4944c1c9967b9c852df54407b5b993e3552d0a56
Favour <georgechiamaka02@gmail.com>
2026-04-08 00:55:10 +0100

    updated payment conntroller

--- routers/paymentRouter.js
+++ routers/paymentRouter.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { isUser } = require("../middlewares/auth");
 const paymentRouter = express.Router();
 const { initializePayment, verifyPayment } = require("../controllers/paymentController");
-
+const paymentController = require('../controllers/paymentController');
 paymentRouter.post("/initializePayment", isUser, initializePayment);
-paymentRouter.get("/verifyPayment", isUser, verifyPayment);
+// Ensure it looks like this:
+paymentRouter.get("/verifyPayment", paymentController.verifyPayment);
 module.exports = paymentRouter;
