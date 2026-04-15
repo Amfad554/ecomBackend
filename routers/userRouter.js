1a352155efa2484181c575b137cc33c23c73e83c
Favour <georgechiamaka02@gmail.com>
2026-04-16 00:53:03 +0100

    updated cart

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -6,7 +6,7 @@ const multer = require('multer');
 
 // 1. IMPORT YOUR AUTH MIDDLEWARE
 // (Assuming your middleware file is named authMiddleware.js)
-const { isUser, isAdmin } = require('../middleware/authMiddleware');
+const { isUser, isAdmin } = require('../middleware/auth'); // Matches auth.js
 
 // 2. EXTRACT getMe FROM YOUR CONTROLLER
 const { registerUser, loginUser, verifyEmail, getAllUsers, deleteUser, updateRole, updateProfile, getMe } = userController;
